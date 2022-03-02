06d71896765c95abfcb73c53bfaa932053b736ec
sushant sharma <rsushant280@gmail.com>
2022-03-02 22:17:44 +0530

    initial commit

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Header from "./header";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ tinder-clone/src/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+
+
+function header() {
+  return (
+    <div className="header">
+      <h1>hell  o i am s  ushant and i am hungry
+        
+
+      </h1>
+      
+      
+    </div>
+  )
+}
+
+export default header
